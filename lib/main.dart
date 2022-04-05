e527d267e822f72ce441a2a0893c74acefb6ad6b
yulio64neos <julio64neos@gmail.com>
2022-04-05 16:03:33 -0500

    Se hizo los cambios, agregando un listado infinito de palabras RANDOM

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,46 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return MaterialApp(
-      title: 'Bienvenido a Flutter',
+    /*final wordPair = WordPair.random();*/
+    return const MaterialApp(
+      title: 'Statup Name Generator',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Bienvenido a Flutter'),
-        ),
-        /*body: const Center(
-          child: Text('Hola Mundo'),
-        ),*/
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
-      ),
+      home: RandomWords()
     );
   }
 }
 
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
 
+class _RandomWordsState extends State<RandomWords>{
+  ///Se declaran dos variables de tipo final
+  ///Una es donde almacenará una colección de palabras
+  final _suggestions = <WordPair>[];
+  ///Y la otra es donde le dará el tamaño de la letra
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Statup Name Generator'),
+      ),
+      body: ListView.builder(padding: const EdgeInsets.all(16.0), itemBuilder: (context, i){
+        if(i.isOdd) return const Divider();
+        final index = i ~/ 2;
+        if(index >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      }),
+    );
+  }
+}
